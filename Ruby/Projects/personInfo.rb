d9117b69aded4fb948620a3b53a9b56394a3ee36
ActorAsher <akhterasher@gmail.com>
2016-09-13 12:57:00 -0500

    Add files via upload

---
+++ Ruby/Projects/personInfo.rb
@@ -0,0 +1,26 @@
+puts "Input your name"
+name = gets.chomp
+
+puts "Input your date of birth"
+birthDate = gets.chomp
+
+puts "Input your email"
+email = gets.chomp
+
+#store in file (personInfo)
+puts "This is your name: #{name}"
+puts "This is your date of birth: #{birthDate}"
+puts "This is your email: #{email}"
+
+puts "Input your email to get your information"
+emailCheck = gets.chomp
+
+# f = File.new("userInfo, r")
+# file.path etc
+
+if emailCheck == email
+  puts "This is your name: #{name}"
+  puts "This is your date of birth: #{birthDate}"
+else
+  puts "The email is not in the system to pull up your information"
+end
